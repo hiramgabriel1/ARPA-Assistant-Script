7efeb1cabe1f3d1eacc870f5c0661ed55b8101b2
itsrusty <82051017+itsrusty@users.noreply.github.com>
2022-10-27 17:51:11 -0700

    Add files via upload

---
+++ ARPA/ARPA/model/actions.py
@@ -0,0 +1,5 @@
+questions = [
+    "el nombre que mi creador me puso fue, en memoria de un gran amigo el cual le dió compañia durante un    tiempo  cuando estaba solo, el murió hace un año, y ahora mi creador hiram, usó el nombre de arpa porque era su sobrenombre"
+]
+
+startAppMessage = ["Hola, bienvenido denuevo, hiram", "¿Que puedo hacer por ti?", "lo siento, no escuché lo que dijiste"]
---
+++ ARPA/ARPA/t.py
@@ -0,0 +1,4 @@
+n = range(0,200)
+for x in n:
+    result = x*3
+    print(result-397)
